e6bb23cf57e68602a6b45bd406bc377ccef591c2
mehmetkesik <mhmtkskx@gmail.com>
2025-05-08 17:15:19 +0300

    updated

--- rshtml/src/parser/text.rs
+++ rshtml/src/parser/text.rs
@@ -9,8 +9,6 @@ pub struct TextParser;
 impl TextParser {
     pub fn remove_extra_newlines(text: &str) -> String {
         let text = text.replace("\r\n", "\n");
-        //let re = Regex::new(r"\n+").unwrap();
-        //re.replace_all(&text, "\n").into_owned()
         let re = Regex::new(r"\n{3,}").unwrap();
         re.replace_all(&text, "\n\n").into_owned()
     }
