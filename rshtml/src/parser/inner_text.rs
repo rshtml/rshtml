6d4e8a169c78997658532be2c331691741c198ad
mk <mhmtkskx@gmail.com>
2025-05-10 14:51:20 +0300

    updated.

--- rshtml/src/parser/inner_text.rs
+++ rshtml/src/parser/inner_text.rs
@@ -1,5 +1,4 @@
 ﻿use crate::Node;
-use crate::parser::text::TextParser;
 use crate::parser::{IParser, RsHtmlParser, Rule};
 use pest::error::Error;
 use pest::iterators::Pair;
@@ -9,9 +8,6 @@ pub struct InnerTextParser;
 impl IParser for InnerTextParser {
     fn parse(_: &mut RsHtmlParser, pair: Pair<Rule>) -> Result<Node, Error<Rule>> {
         let inner_text = pair.as_str().replace("@@", "@").replace("@@{", "{").replace("@@}", "}");
-
-        let inner_text = TextParser::remove_extra_newlines(&inner_text);
-
         Ok(Node::InnerText(inner_text))
     }
 }
--- rshtml/src/parser/text.rs
+++ rshtml/src/parser/text.rs
@@ -2,25 +2,12 @@
 use crate::parser::{IParser, RsHtmlParser, Rule};
 use pest::error::Error;
 use pest::iterators::Pair;
-use regex::Regex;
 
 pub struct TextParser;
-
-impl TextParser {
-    // Maybe delete this function for <pre> tag or something
-    pub fn remove_extra_newlines(text: &str) -> String {
-        let text = text.replace("\r\n", "\n");
-        let re = Regex::new(r"\n{3,}").unwrap();
-        re.replace_all(&text, "\n\n").into_owned()
-    }
-}
 
 impl IParser for TextParser {
     fn parse(_: &mut RsHtmlParser, pair: Pair<Rule>) -> Result<Node, Error<Rule>> {
         let text = pair.as_str().replace("@@", "@").replace("@@{", "{").replace("@@}", "}");
-
-        let text = Self::remove_extra_newlines(&text);
-
         Ok(Node::Text(text))
     }
 }
