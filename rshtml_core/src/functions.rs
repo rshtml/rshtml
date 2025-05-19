6dc9f6a440fdb512c7e7e6f947f21bc59f946c81
mk <mhmtkskx@gmail.com>
2025-05-20 00:11:50 +0300

    time function developed.

--- rshtml_core/src/functions.rs
+++ rshtml_core/src/functions.rs
@@ -1,3 +1,5 @@
+mod time;
+
 use serde::Serialize;
 
 pub fn functions(layout: String, sections: Vec<String>) -> Functions {
@@ -25,17 +27,30 @@ impl Functions {
 
     pub fn json<T: Serialize>(&self, value: &T) -> String {
         serde_json::to_string(value).unwrap_or_else(|err| {
-            dbg!("DEBUG: JSON error: {}", err);
-            String::new()
+            eprintln!("DEBUG: JSON error: {}", err);
+            "{}".to_string()
         })
+    }
+
+    pub fn json_let<T: Serialize>(&self, name: &str, value: &T) -> String {
+        let json = serde_json::to_string(value).unwrap_or_else(|err| {
+            eprintln!("DEBUG: JSON error: {}", err);
+            "{}".to_string()
+        });
+
+        format!("let {} = {}", name, json)
+    }
+
+    pub fn class(&self, classes: &[&str]) -> String {
+        format!("class=\"{}\"", classes.join(" "))
     }
 
     pub fn t(&self, word: &str) -> String {
         todo!()
     }
 
-    pub fn push(&mut self, text: String) {
-        self.pushed_texts.push(text);
+    pub fn push(&mut self, text: &str) {
+        self.pushed_texts.push(text.to_string());
     }
 
     pub fn stack(&self) -> String {
---
+++ rshtml_core/src/functions/time.rs
@@ -0,0 +1,65 @@
+use crate::functions::Functions;
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
+use std::str::FromStr;
+
+// TODO: icu_datetime = "1" and icu_locid = "1" use for localicazation
+
+impl Functions {
+    pub fn time<T: Display + ?Sized>(&self, value: &T) -> RsDateTime {
+        let value_str = value.to_string();
+        let default_format = "%Y-%m-%d %H:%M:%S".to_string();
+
+        match DateTime::parse_from_rfc3339(&value_str) {
+            Ok(dt) => return RsDateTime(dt.with_timezone(&Utc), default_format),
+            Err(err) => eprintln!("DEBUG: Time error: {}", err),
+        };
+
+        match NaiveDateTime::from_str(&value_str) {
+            Ok(ndt) => return RsDateTime(DateTime::<Utc>::from_naive_utc_and_offset(ndt, Utc), default_format),
+            Err(err) => eprintln!("DEBUG: Time error: {}", err),
+        };
+
+        match NaiveDate::from_str(&value_str) {
+            Ok(nd) => {
+                let naive_datetime = nd.and_time(NaiveTime::from_hms_opt(0, 0, 0).unwrap());
+                return RsDateTime(DateTime::<Utc>::from_naive_utc_and_offset(naive_datetime, Utc), default_format);
+            }
+            Err(err) => eprintln!("DEBUG: Time error: {}", err),
+        };
+
+        RsDateTime(DateTime::default(), default_format)
+    }
+}
+
+pub struct RsDateTime(DateTime<Utc>, String);
+
+impl RsDateTime {
+    pub fn date_time(&self) -> DateTime<Utc> {
+        self.0
+    }
+
+    pub fn date(&self) -> NaiveDate {
+        self.0.date_naive()
+    }
+
+    pub fn pretty(&mut self) -> &Self {
+        self.1 = "%b %d, %Y".to_string();
+        self
+    }
+}
+
+impl Display for RsDateTime {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.format(self.1.as_str()))
+    }
+}
+
+impl Deref for RsDateTime {
+    type Target = DateTime<Utc>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
