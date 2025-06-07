eb45e7743aa8f6ba430573029e3ff6315a16ddd2
mk <mhmtkskx@gmail.com>
2025-06-08 01:14:14 +0300

    updated

--- rshtml/src/lib.rs
+++ rshtml/src/lib.rs
@@ -1,7 +1,7 @@
 //! [![Crates.io Version](https://img.shields.io/crates/v/rshtml.svg)](https://crates.io/crates/rshtml)
 //! [![GitHub Repository](https://img.shields.io/badge/github-repo-blue.svg?logo=github)](https://github.com/mehmetkesik/rshtml)
 //! [![Docs.rs Documentation](https://docs.rs/rshtml/badge.svg)](https://docs.rs/rshtml)
-//! [![Full Documentation](https://img.shields.io/badge/website-rshtml.gitlab.io-blue.svg)](https://mehmetkesik.gitlab.io/rshtml/)
+//! [![Full Documentation](https://img.shields.io/badge/book-rshtml.github.io-blue.svg)](https://rshtml.github.io/)
 //!
 //! # RsHtml: A Template Engine for Seamless HTML and Rust Integration.
 //!
