5d6f39967dd12a1563081611736febb8f1176a74
mehmetkesik <mhmtkskx@gmail.com>
2025-09-24 20:23:10 +0300

    temporary file extracting developed

--- rshtml_core/src/lib.rs
+++ rshtml_core/src/lib.rs
@@ -7,7 +7,7 @@ mod node;
 mod parser;
 mod position;
 pub mod str_extensions;
-// mod temporary_file_writer;
+mod temporary_file;
 #[cfg(test)]
 mod tests;
 
@@ -23,8 +23,7 @@ use std::clone::Clone;
 pub fn process_template(template_name: String, struct_name: &Ident) -> TokenStream {
     let config = Config::load_from_toml_or_default();
     let layout = config.layout.clone();
-
-    dbg!(config.clone());
+    let extract_file_on_debug = config.extract_file_on_debug.clone();
 
     let (compiled_ast_tokens, sections, text_size) = match parse_and_compile(&template_name, config)
     {
@@ -72,24 +71,14 @@ pub fn process_template(template_name: String, struct_name: &Ident) -> TokenStre
         };
     };
 
-    // use temporary_file_writer::write;
-    // let temporary_file = write(
-    //     struct_name.to_string().as_str(),
-    //     generated_code.to_string().as_str(),
-    // );
-    // let temporary_file = temporary_file.unwrap();
-    // let temporary_file_str = temporary_file.to_string_lossy().to_string();
-
-    // std::process::Command::new("rustfmt")
-    //     .arg("--config")
-    //     .arg("max_width=10000")
-    //     .arg(temporary_file)
-    //     .status()
-    //     .expect("rustfmt komutu çalıştırılamadı.");
-
-    // let generated_code = quote! {
-    //     include!(#temporary_file_str);
-    // };
+    if cfg!(debug_assertions) && extract_file_on_debug {
+        match temporary_file::create(&struct_name.to_string(), &generated_code.to_string()) {
+            Ok(code) => return code,
+            Err(err) => {
+                dbg!("Failed to create temporary file:", err);
+            }
+        }
+    }
 
     generated_code
 }
