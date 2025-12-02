edad63dc62fca4f976a6d4b986413d234a579014
mehmetkesik <mhmtkskx@gmail.com>
2025-12-02 18:14:38 +0300

    updated

--- rshtml_test/src/lib.rs
+++ rshtml_test/src/lib.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 use std::fmt::Write;
 
 #[derive(RsHtml)]
-#[rshtml(path = "home.rs.html"/*, no_warn*/)]
+// #[rshtml(path = "home.rs.html", no_warn)]
 pub struct HomePage {
     pub title: String,
     pub content: String,
