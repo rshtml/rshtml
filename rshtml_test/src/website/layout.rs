50beb54ace4422922e92307a673827dc5fca7fd9
mehmetkesik <mhmtkskx@gmail.com>
2026-01-19 17:21:29 +0300

    update

--- rshtml_test/src/website/layout.rs
+++ rshtml_test/src/website/layout.rs
@@ -1,10 +1,9 @@
-use rshtml::{traits::View, v};
-
 use crate::website::scripts::scripts;
+use rshtml::{traits::View, v};
 
 pub fn layout(title: &str, navbar: impl View, footer: impl View, content: impl View) -> impl View {
     v! {
-        // <!DOCTYPE html>
+        <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="utf-8" />
@@ -13,18 +12,18 @@ pub fn layout(title: &str, navbar: impl View, footer: impl View, content: impl V
 
             <title>Index - {title}</title>
 
-            // <!-- load stylesheets -->
+            <!-- load stylesheets -->
             <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400"/>
-            // <!-- Google web font "Open Sans" -->
+            <!-- Google web font "Open Sans" -->
             <link rel="stylesheet" href="css/bootstrap.min.css"/>
             <link rel="stylesheet" href="css/style.css"/>
 
-            // <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            // <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-            // <!--[if lt IE 9]> -->
+            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+            <!-- WARNING: Respond.js does not work if you view the page via file:// -->
+            <!--[if lt IE 9]> -->
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            // <!--<![endif]-->
+            <!--<![endif]-->
         </head>
         <body>
 
