d71e34fe75a793c4b710f7c77b9b87e480640938
mehmetkesik <mhmtkskx@gmail.com>
2026-02-16 22:46:06 +0300

    update

--- rshtml_core/src/extract_file.rs
+++ rshtml_core/src/extract_file.rs
@@ -2,16 +2,28 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use std::fs;
 use std::io;
+use std::path::Component;
 use std::path::Path;
 use std::path::PathBuf;
 
 pub fn create(path: &Path, generated_code: &str) -> io::Result<TokenStream> {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| ".".to_string());
+
     let mut target_path = PathBuf::from(&manifest_dir);
     target_path.push("target");
     target_path.push("rshtml");
 
-    target_path.push(path);
+    for component in path.components() {
+        match component {
+            Component::Normal(c) => {
+                target_path.push(c);
+            }
+            Component::ParentDir => {
+                target_path.push("__up__");
+            }
+            Component::RootDir | Component::Prefix(_) | Component::CurDir => {}
+        }
+    }
 
     let parent = target_path.parent().ok_or(io::Error::new(
         io::ErrorKind::InvalidData,
@@ -20,11 +32,6 @@ pub fn create(path: &Path, generated_code: &str) -> io::Result<TokenStream> {
     fs::create_dir_all(parent)?;
 
     fs::write(&target_path, generated_code)?;
-
-    let mut include_path = PathBuf::new();
-    include_path.push("target");
-    include_path.push("rshtml");
-    include_path.push(path);
 
     let include_path_str = target_path.to_str().ok_or(io::Error::new(
         io::ErrorKind::InvalidData,
